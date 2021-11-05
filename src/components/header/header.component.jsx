4e548eef808dd339ea41cfa7b3ba149088666721
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2021-11-05 03:35:15 +0100

    implemented firebase utils, included ability to authenticate users into firestore database

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -15,7 +15,7 @@ const Header = ({currentUser}) => (
       <Link className="option" to='/shop'>SHOP</Link>
       <Link className="option" to='/shop'>CONTACT</Link>
       {
-        currentUser
+        currentUser !== null
           ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
           : <Link className='option' to='/signin'>SIGN IN</Link>
       }
